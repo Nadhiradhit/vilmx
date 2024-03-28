76c19b31991967a208bb0cb371101432647497c5
Nadhiradhit <nadhiradhitya@gmail.com>
2024-03-28 13:09:27 +0700

    refactor: fixing navbar mobile

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,10 +1,10 @@
-import Footer from "@/components/footer/footer";
+// import Footer from "@/components/footer/footer";
 import { PropsWithChildren } from "react";
 function Layout(props: PropsWithChildren) {
 	return (
 		<>
 			{props.children}
-			<Footer />
+			{/* <Footer /> */}
 		</>
 	);
 }
